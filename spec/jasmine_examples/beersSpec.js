175c6ba24c6142be5bde324a421b961f8e4f33d0
exphuhong <177732943@qq.com>
2017-11-29 18:28:29 +0800

    test 2

--- spec/jasmine_examples/beersSpec.js
+++ spec/jasmine_examples/beersSpec.js
@@ -4,4 +4,10 @@ describe("beers",function(){
     it("99 bottles of beers",function(){
         expect(beers(99)).toEqual("99 bottles of beer on the wall, 99 bottles of beer.Take one down and pass it around, 98 bottles of beer on the wall.");
     })
+    it("1 bottle of beers",function(){
+        expect(beers(1)).toEqual("1 bottle of beer on the wall, 1 bottle of beer."+
+        "Take one down and pass it around, no more bottles of beer on the wall."+
+        "No more bottles of beer on the wall, no more bottles of beer."+
+        "Go to the store and buy some more, 99 bottles of beer on the wall.");
+    })
 })
